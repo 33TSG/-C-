4e63865e4810955785aaf74969c9377aa9bb2016
13132 <zhangangzhi@outlook.com>
2023-03-13 20:40:11 +0800

    拓扑排序

--- 源.cpp
+++ 源.cpp
@@ -1,85 +1,161 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
-#include <algorithm>
-
+#include<iostream>
+#include<string>
 using namespace std;
+#define MVNum 100
+#define OK 1
+#define ERROR 0
+#define MaxInt 100
+typedef string VerTexType;
+typedef int Status;
+typedef int SElemType;
 
-class Graph {        // 无向图
-public:
-	int v, e;       // v 是顶点数， e是边的数目
-	vector<vector<int>> matrix;        //邻接矩阵
+typedef struct {
+	SElemType* base;
+	SElemType* top;
+	int stacksize;
+}SqStack;
 
-	Graph(int v, int e) : v(v), e(e) {
-		matrix = vector<vector<int>>(v, vector<int>(v, 0));
-		for (int i = 0; i < v; ++i) {
-			cout << "请输入相邻的两个顶点及连接边上的权值";
-			int a, b, power;            // 两个顶点和权值
-			cout << endl;
-			cin >> a >> b >> power;
-			matrix[a][b] = matrix[b][a] = power;
-		}
+typedef struct ArcNode {
+	int adjvex;
+	struct ArcNode* nextarc;
+}ArcNode;
+typedef struct VNode {
+	VerTexType data;
+	ArcNode* firstarc;
+}VNode, AdjList[MVNum];
+
+typedef struct {
+	int vexnum, arcnum;
+	AdjList vertices;
+}ALGraph;
+
+/*--------拓扑排序辅助数组的存储结构--------*/
+int indegree[MVNum];//存放各顶点入度
+int topo[MVNum];//记录拓扑序列的顶点编号
+Status InitStack(SqStack& S) {
+	S.base = new SElemType[MaxInt];
+	if (!S.base) return ERROR;
+	S.top = S.base;
+	S.stacksize = MaxInt;
+	return OK;
+}
+
+Status StackEmpty(SqStack S) {
+	if (S.top == S.base) return OK;
+	return ERROR;
+}
+
+Status Push(SqStack& S, SElemType e) {
+	if (S.top - S.base == S.stacksize) return ERROR;
+	*S.top = e;
+	S.top++;
+	return OK;
+}
+
+Status Pop(SqStack& S, SElemType& e) {
+	if (S.base == S.top) return ERROR;
+	S.top--;
+	e = *S.top;
+	return OK;
+}
+
+int LocateVex(ALGraph G, VerTexType v) {
+	for (int i = 0; i < G.vexnum; i++) {
+		if (G.vertices[i].data == v)
+			return i;
 	}
+	return -1;
+}
 
-	vector<vector<int>> prim() {
-		vector<vector<int>> res(v, vector<int>(v, 0));
-		vector<int> temp;               // 保存已有的顶点
-		bool* exists = new bool[v];     //顶点是否已经在集合里
-		exists[0] = true;               //从第一个顶点开始生成
-		temp.push_back(0);
+void CreateUDG(ALGraph& G) {
+	cin >> G.vexnum >> G.arcnum;
+	for (int i = 0; i < G.vexnum; i++) {
+		cin >> G.vertices[i].data;
+		G.vertices[i].firstarc = NULL;//初始化表头结点的指针域为NULL
+	}
+	for (int k = 0; k < G.arcnum; k++) {
+		VerTexType v1, v2;
+		cin >> v1 >> v2;
+		int i = LocateVex(G, v1);
+		int j = LocateVex(G, v2);
+		ArcNode* p1 = new ArcNode;
+		p1->adjvex = j;
+		p1->nextarc = G.vertices[i].firstarc;
+		G.vertices[i].firstarc = p1;
+	}
+}
 
-		while (temp.size() != v) {
-			int dist = 1000, mark1 = 0, mark2 = 0;
-			for (int i : temp) {
-				for (int j = 0; j < v; ++j) {
-					if (!exists[j] && matrix[i][j] != 0 && dist > matrix[i][j]) {
-						dist = matrix[i][j];
-						mark1 = i;
-						mark2 = j;
-					}
-				}
+void FindInDegree(ALGraph G, int indegree[]) {
+	for (int i = 0; i < G.vexnum; i++) {
+		int cnt = 0;//设置变量存储邻接点域为i的结点个数
+		for (int j = 0; j < G.vexnum; j++) {
+			ArcNode* p = new ArcNode;//定义指向各个边结点的指针
+			p = G.vertices[j].firstarc;
+			while (p) {//当p未指到单个链表的末尾时继续循环
+				if (p->adjvex == i)//当某边结点邻接点域等于i时，计数变量++
+					cnt++;
+				p = p->nextarc;//指针不断向后指
 			}
-			exists[mark2] = true;
-			temp.push_back(mark2);
-			res[mark1][mark2] = res[mark2][mark1] = matrix[mark1][mark2];
-		}
-		return res;
-	}
-
-	void print(vector<vector<int>> Matrix) {
-		for (int i = 0; i < v; ++i) {
-			for (int j = 0; j < v; ++j) {
-				cout << Matrix[i][j] << "  ";
-			}
-			cout << endl;
+			indegree[i] = cnt;//将计数结果保留在indegree数组中
 		}
 	}
-};
+}
 
-/*
- * 7  10
- * 0 1 5
- * 0 2 7
- * 0 6 2
- * 1 3 9
- * 1 6 3
- * 4 2 8
- * 4 6 4
- * 4 5 5
- * 5 6 6
- * 5 3 4
- */
+Status TopologicalSort(ALGraph G, int topo[]) {
+	//有向图G采用邻接表存储结构
+	//若G无回路，则生成G的一个拓扑排序topo[]并返回OK，否则ERROR
+	FindInDegree(G, indegree);//求出各结点的入度存入数组indegree中
+	SqStack S;
+	InitStack(S);//初始化栈
+	for (int i = 0; i < G.vexnum; i++) {
+		if (!indegree[i]) Push(S, i);//入度为0者进栈
+	}
+	int m = 0;//对输出顶点计数u，初始为0
+	while (!StackEmpty(S)) {
+		int i = 0;
+		Pop(S, i);//将栈顶顶点vi出栈
+		topo[m] = i;//将vi保存在拓扑序列数组topo中
+		++m;//对输出顶点计数
+		ArcNode* p = new ArcNode;
+		p = G.vertices[i].firstarc;//p指向vi的第一个邻接点
+		while (p != NULL) {
+			int k = p->adjvex;//vk为vi的邻接点
+			--indegree[k];//vi的每个邻接点的入度减一
+			if (indegree[k] == 0) Push(S, k);//若入度减为0，则入栈
+			p = p->nextarc;//p指向顶点vi下一个邻接结点
+		}
+	}
+	if (m < G.vexnum) return ERROR;//该有向图有回路
+	else return OK;
+}
+
+/*输出拓扑排序后的结果*/
+void PrintResult(ALGraph G) {
+	if (TopologicalSort(G, topo)) {
+		for (int i = 0; i < G.vexnum; i++) {
+			cout << G.vertices[topo[i]].data << " ";
+		}
+	}
+}
 
 int main() {
-	int m, n;
-	cout << "输入图的顶点数和边数：" << endl;
-	cin >> m >> n;
-	Graph g(m, n);
-	cout << "prim算法最小生成树的邻接矩阵" << endl;
-	g.print(g.prim());
+	ALGraph G;
+	CreateUDG(G);
+	PrintResult(G);
 	return 0;
 }
+
+/*测试数据
+输入：
+6 8
+v1 v2 v3 v4 v5 v6
+v1 v2
+v1 v3
+v1 v4
+v3 v2
+v3 v5
+v4 v5
+v6 v4
+v6 v5
+
+*/
