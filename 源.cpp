10ae05d25b0c4a29992420c168cd03b8fc1409bb
13132 <zhangangzhi@outlook.com>
2023-04-01 21:47:31 +0800

    实验2.4

--- 源.cpp
+++ 源.cpp
@@ -1,77 +1,60 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#define Max 110
 
-class Color
-{
-	friend int mColoring(int, int, int**);
-public:
-	bool Ok(int k);
-	void Backtrack(int t);
-	int n, m, **a, *x;
-	long sum;
-};
+int n1, n2, m, t;
+int match[Max][Max], f[Max][Max], g[Max][Max];
+int s1[Max], s2[Max];
 
-int mColoring(int n, int m, int** a);
+int max(int a, int b) {
+	return a > b ? a : b;
+}
 
 int main() {
-	int n, m, ** a, sum;
-	cout << "Please input the number of colors:";
-	cin >> m;
-	cout << "Please input the number of nodes:";
-	cin >> n;
-	a = new int* [n + 1];
-	for (int i = 0; i <= n; i++)
-		a[i] = new int[n + 1];
-	cout << "Please input the relation of the nodes 1-- > Connected, 0-- > Not connected : " << endl;
-	for (int i = 1; i <= n; i++){
-		for (int j = i + 1; j <= n; j++) {
-			cout << "Please input the relation of " << i << " and " << j << ": ";
-			cin >> a[i][j];
-			a[j][i] = a[i][j];
+	int i, j, k;
+	int ii, jj;
+	scanf_s("%d", &m);
+	while (m) {
+		m--;
+		scanf_s("%d %d", &n1, &n2);
+		for (i = 1; i <= n1; i++)
+			scanf_s("%d", &s1[i]);
+		for (i = 1; i <= n2; i++)
+			scanf_s("%d", &s2[i]);
+		memset(f, 0, sizeof(f));
+		memset(g, 0, sizeof(g));
+		for (i = 1; i <= n1; i++) {
+			for (j = 1; j <= n2; j++) {
+				if (s1[i] == s2[j - 1])
+					f[i][j] = j - 1;
+				else 
+					f[i][j] = f[i][j-1];
+			}
 		}
+		for (i = 1; i <= n2; i++) {
+			for (j = 1; j <= n1; j++) {
+				if (s2[i] == s1[j - 1])
+					g[i][j] = j - 1;
+				else
+					g[i][j] = g[i][j - 1];
+			}
+		}
+		memset(match, 0, sizeof(match));
+		for (i = 1; i <= n1; i++) {
+			for (j = 1; j <= n2; j++) {
+				match[i][j] = max(match[i][j-1], match[i-1][j]);
+				if (s1[i] == s2[j])
+					continue;
+				ii = f[i][j];
+				jj = g[j][i];
+				if (ii == 0 || jj == 0)
+					continue;
+				match[i][j] = max(match[i][j], match[jj-1][ii-1]+2);
+			}
+		}
+		printf("%d", match[n1][n2]);
 	}
-	sum = mColoring(n, m, a);
 	return 0;
-}
-
-bool Color::Ok(int k) {
-	for (int j = 1; j <= n; j++)
-		if (a[k][j] == 1 && x[j] == x[k])
-			return false;
-	return true;
-}
-
-void Color::Backtrack(int t) {
-	if (t > n) {
-		sum++;
-		for (int i = 1; i <= n; i++)
-		{
-			cout << x[i] << " ";
-		}
-		cout << endl;
-	}
-	else {
-		for (int i = 1; i <= m; i++) {
-			x[t] = i;
-			if (Ok(t))
-				Backtrack(t+1);
-			x[t] = 0;
-		}
-	}
-}
-
-int mColoring(int n, int m, int** a) {
-	Color X;
-	X.n = n;
-	X.m = m;
-	X.a = a;
-	X.sum = 0;
-	int* p = new int[n + 1];
-	for (int i = 0; i <= n; i++) {
-		p[i] = 0;
-	}
-	X.x = p;
-	X.Backtrack(1);
-	delete[] p;
-	return X.sum;
 }
