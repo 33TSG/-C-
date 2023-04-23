f6e1b3eee1c74e4a06caa3ea56b6554804443170
13132 <zhangangzhi@outlook.com>
2023-04-23 23:08:48 +0800

    实验5.1递归版本

--- 源.cpp
+++ 源.cpp
@@ -1,52 +1,62 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 int vis[101][101];
 int map[101][101];
 int R, C;
 
-int dp(int i, int j) {
-	int max = 0;
-	if (vis[i][j] > 0)
-		return vis[i][j];
-	if (i - 1 >= 0)
-		if (map[i - 1][j] < map[i][j])
-			if (max < dp(i - 1, j))
-				max = dp(i - 1, j);
+int dfs(int i, int j) {
+    if (vis[i][j] > 0) {
+        return vis[i][j];
+    }
 
-	if (i + 1 < R)
-		if (map[i + 1][j] < map[i][j])
-			if (max < dp(i + 1, j))
-				max = dp(i + 1, j);
+    int max = 1;
 
-	if (j-1 >= 0) 
-		if (map[i][j-1] < map[i][j])
-			if (max < dp(i, j-1))
-				max = dp(i, j-1);
+    if (i - 1 >= 0 && map[i - 1][j] < map[i][j]) {
+        int tmp = dfs(i - 1, j) + 1;
+        max = tmp > max ? tmp : max;
+    }
 
-	if (j+1 <= C) 
-		if (map[i][j+1] < map[i][j])
-			if (max < dp(i, j+1))
-				max = dp(i, j+1);
+    if (i + 1 < R && map[i + 1][j] < map[i][j]) {
+        int tmp = dfs(i + 1, j) + 1;
+        max = tmp > max ? tmp : max;
+    }
 
-	return vis[i][j] = max+1;
+    if (j - 1 >= 0 && map[i][j - 1] < map[i][j]) {
+        int tmp = dfs(i, j - 1) + 1;
+        max = tmp > max ? tmp : max;
+    }
+
+    if (j + 1 < C && map[i][j + 1] < map[i][j]) {
+        int tmp = dfs(i, j + 1) + 1;
+        max = tmp > max ? tmp : max;
+    }
+
+    vis[i][j] = max;
+
+    return max;
 }
 
 int main() {
-	int i, j, ans, max;
-	scanf_s("%d%d", &R, &C);
-	for (i = 0; i < R; i++)
-		for (j = 0; j < C; j++)
-			scanf_s("%d", &map[i][j]);
-	max = 0;
-	for (i = 0; i < R; i++) {
-		memset(vis[i], -1, sizeof(vis[i]));
-		for (j = 0; j < C; j++) {
-			ans = dp(i, j);
-			if (ans > max)
-				max = ans;
-		}
-	}
-	printf("%d", max);
-	return 0;
+    int i, j, ans, max;
+    scanf_s("%d %d", &R, &C);
+
+    for (i = 0; i < R; i++) {
+        for (j = 0; j < C; j++) {
+            scanf_s("%d", &map[i][j]);
+        }
+    }
+
+    max = 1;
+
+    for (i = 0; i < R; i++) {
+        for (j = 0; j < C; j++) {
+            ans = dfs(i, j);
+            max = ans > max ? ans : max;
+        }
+    }
+
+    printf("%d", max);
+
+    return 0;
 }
