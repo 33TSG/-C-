9fef7a7e45c197ea2cd461f93ac9a4f2a6818a45
13132 <zhangangzhi@outlook.com>
2023-05-02 18:59:02 +0800

    作业4   问题1

--- 源.cpp
+++ 源.cpp
@@ -1,28 +1,48 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
 
 using namespace std;
 
-int main() {
-	int w[5] = { 0 , 2 , 3 , 4 , 5 };			//商品的体积2、3、4、5
-	int v[5] = { 0 , 3 , 4 , 5 , 6 };			//商品的价值3、4、5、6
-	int bagV = 8;					        //背包大小
-	int dp[5][9] = { { 0 } };
-	
-	for (int i = 1; i <= 4; i++)
-	{
-		for (int j = 1; j <= 8; j++) {
-			if (w[i] > j) {
-				dp[i][j] = dp[i - 1][j];
-			}
-			else {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+// 回溯法求结果
+void traceBack(vector<string>& words, string aim, int l, int k, int &ans) {
+	if (aim.size() == k) {
+		cout << aim << endl;
+		ans++;
+	}
+	else {
+		for (int i = 0; i < words.size(); i++) {
+			string a = aim.substr(aim.length() - l + 1, l - 1);
+			string b = words[i].substr(0, l - 1);
+			if (a == b) {
+				aim += words[i][l-1];
+				traceBack(words, aim, l, k, ans);
+				aim.pop_back();
 			}
 		}
 	}
+}
 
-	cout << dp[4][8] << endl;
+int main() { 
+	int m;		//m表示表中单词的个数
+	int n;		//n表示不同字母的最大个数
+	int k;		//k表示最后构成的字符串的长度
+	int ans{ 0 };	// 能够构成的满足条件的字符串的个数
+
+	cin >> m >> n >> k;
+	vector<string> words;	// 所有的单词
+	for (size_t i = 0; i < m; i++)
+	{	
+		string s;
+		cin >> s;
+		words.push_back(s);
+	}
+
+	for (size_t i = 0; i < words.size(); i++)
+	{
+		traceBack(words, words[i], words[i].size(), k, ans);
+	}
+
+	cout << ans;
 
 	return 0;
 }
