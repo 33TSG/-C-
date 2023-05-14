4f15560a78a95e21d1886de9b3d0832c179564cc
13132 <zhangangzhi@outlook.com>
2023-05-14 09:39:36 +0800

    实验6.1

--- 源.cpp
+++ 源.cpp
@@ -1,81 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#define MAX 100000
-
+#include <string>
 using namespace std;
 
-struct Brick {
-    int price;
-    int copper;
-};
-
-// 递归函数，用于计算组合数
-void combination(vector<int>& nums, int k, vector<int>& path, vector<vector<int>>& res, int start) {
-    // 判断递归终止条件，如果 path 中已经有了 k 个元素，说明找到了一组组合
-    if (path.size() == k) {
-        res.push_back(path); // 将找到的组合存储到 res 中
-        return;
+string removeKdigits(string num, int k) {
+    // num = "1432219" k = 3
+    string res;
+    int n = num.size(), keep = n - k;
+    for (char c : num) {
+        while (k > 0 && !res.empty() && res.back() > c) {
+            res.pop_back();
+            k--;
+        }
+        res.push_back(c);
     }
-    // 从 start 开始遍历 nums
-    for (int i = start; i < nums.size(); i++) {
-        path.push_back(nums[i]); // 将当前元素加入 path 中
-        combination(nums, k, path, res, i + 1); // 进行下一层递归，start 设为 i+1，避免重复选取同一元素
-        path.pop_back(); // 回溯到上一层递归，将最后添加的元素弹出
-    }
-}
-
-int sumCopper(vector<int>& nums, vector<Brick> &bricks) {
-    int sum{ 0 };
-    for (int i : nums)
-        sum += bricks[i].copper;
-    return sum;
-}
-
-int sumPrice(vector<int>& nums, vector<Brick>& bricks) {
-    int sum{ 0 };
-    for (int i : nums)
-        sum += bricks[i].price;
-    return sum;
+    res.resize(keep);
+    while (!res.empty() && res.front() == '0') res.erase(res.begin());
+    return res.empty() ? "0" : res;
 }
 
 int main() {
-    int n; // 工厂生产的砖种数
-    cin >> n;
-
-    vector<Brick> bricks(n + 1);
-    for (int i = 1; i <= n; i++) {
-        cin >> bricks[i].copper >> bricks[i].price;
-    }
-
-    int c; // 顾客数量
-    cin >> c;
-
-    while (c--) {
-        int m, cmin, cmax;
-        cin >> m >> cmin >> cmax;
-
-        int ans = MAX;
-
-        vector<int> path;
-        vector<vector<int>> res;
-        vector<int> nums(n);
-        for (int i = 0; i < n; i++) {
-            nums[i] = i + 1; // 初始化 nums 数组为 1,2,3,...,n
-        }
-        combination(nums, m, path, res, 0);         // 调用递归函数求组合数
-
-        for (auto &i: res) {
-            if (sumCopper(i, bricks) >= cmin && sumCopper(i, bricks) <= cmax)
-                ans = min(ans, sumPrice(i, bricks));
-        }
-
-        if (ans != MAX)
-            cout << ans << endl;
-        else
-            cout << "NO" << endl;
-
-    }
-
+    string num;
+    int k;
+    cout << "Please enter a non-negative integer: ";
+    cin >> num;
+    cout << "Please enter the number of digits to be removed: ";
+    cin >> k;
+    cout << "The smallest number with " << k << " digits removed is: " << removeKdigits(num, k) << endl;
     return 0;
 }
+
